85e8e5ae5f862773e201306a73a7a3616f5845b7
oyin8040 <morayoadewale18@gmail.com>
2026-03-31 20:51:23 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Backend is working 🚀");
+});
+
+app.get("/api/test", (req, res) => {
+  res.json({ message: "API is working" });
 });
 
 const PORT = process.env.PORT || 10000;
