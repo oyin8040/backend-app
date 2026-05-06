d2a80dc444abf25ef45964b2192f3be54b34c311
oyin8040 <morayoadewale18@gmail.com>
2026-05-07 00:26:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
-app.use(express.json());
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send("Backend is working 🚀");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.get("/api/test", (req, res) => {
-  res.json({ message: "API is working" });
-});
-
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});
+}); 
