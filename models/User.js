25d99d2f86b380f69b988a42639cb76c52aa2f10
oyin8040 <morayoadewale18@gmail.com>
2026-05-07 01:35:18 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,18 @@
+const mongoose = require("mongoose");
 
+const UserSchema = new mongoose.Schema({
+
+    username:String,
+
+    email:String,
+
+    password:String,
+
+    wallet:{
+        type:Number,
+        default:0
+    }
+
+});
+
+module.exports = mongoose.model("User",UserSchema); 
