d795bf73fcc2e168ab7c5bfdac53bbc012a71e17
oyin8040 <morayoadewale18@gmail.com>
2026-05-07 01:36:53 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,1 +1,13 @@
+const express = require("express");
 
+const router = express.Router();
+
+router.post("/signup",(req,res)=>{
+
+    res.json({
+        message:"Signup route working"
+    });
+
+});
+
+module.exports = router; 
