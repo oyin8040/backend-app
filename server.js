94edeef82003d9d8b884032b82d288e7aacbd65a
oyin8040 <morayoadewale18@gmail.com>
2026-05-07 01:37:35 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,20 @@ const path = require("path");
 
 const app = express();
 
+app.use(express.json());
+
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+const authRoutes = require("./routes/auth");
+
+app.use("/api/auth",authRoutes);
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"public","index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT,()=>{
+    console.log("Server running");
 }); 
