c4fcda187b8325f9523fbe30aad0393b01f3f4bb
oyin8040 <morayoadewale18@gmail.com>
2026-05-17 17:09:53 +0100

    Update controllers.js

--- controllers.js
+++ controllers.js
@@ -1,11 +1,32 @@
+const User = require("../models/User");
 
-    const signup = (req,res)=>{
+const signup = async (req,res)=>{
 
-    console.log(req.body);
+    try{
 
-    res.json({
-        message:"Signup successful"
-    });
+        const {username,email,password} = req.body;
+
+        const newUser = new User({
+
+            username,
+            email,
+            password
+
+        });
+
+        await newUser.save();
+
+        res.json({
+            message:"Account created successfully"
+        });
+
+    }catch(error){
+
+        res.json({
+            message:"Error creating account"
+        });
+
+    }
 
 };
 
