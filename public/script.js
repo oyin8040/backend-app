1bcadf1b3c14dc999178696a3c709e813ce5cc73
oyin8040 <morayoadewale18@gmail.com>
2026-05-25 23:15:11 +0100

    Add signup function for user registration
    Implement signup function to handle user registration.

--- public/script.js
+++ public/script.js
@@ -1,1 +1,29 @@
+function signup(){
 
+    const email = document.getElementById("email").value;
+
+    const password = document.getElementById("password").value;
+
+    auth.createUserWithEmailAndPassword(email,password)
+
+    .then((userCredential)=>{
+
+        const user = userCredential.user;
+
+        /* SAVE USER */
+
+        localStorage.setItem("userEmail",user.email);
+
+        /* MOVE TO NEXT PAGE */
+
+        window.location.href = "dashboard.html";
+
+    })
+
+    .catch((error)=>{
+
+        alert(error.message);
+
+    });
+
+}
