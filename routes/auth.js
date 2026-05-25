2f569fc79d1610c02a4486dcceb5c97c74079cc1
oyin8040 <morayoadewale18@gmail.com>
2026-05-25 23:21:21 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,1 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDsdGQL-FYLsQdQipQPa8M0BCb0SHg4UTY",
-  authDomain: "gen-lang-client-0173296700.firebaseapp.com",
-  projectId: "gen-lang-client-0173296700",
-  storageBucket: "gen-lang-client-0173296700.firebasestorage.app",
-  messagingSenderId: "109891504599",
-  appId: "1:109891504599:web:39c7f92da4c6e937436392",
-  measurementId: "G-ZXYWVV4VQF"
-};
+
