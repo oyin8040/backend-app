c3d716f5d41680f5683e27ab8680179726a98fd4
oyin8040 <morayoadewale18@gmail.com>
2026-05-31 14:46:51 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,109 +1,23 @@
-const firebaseConfig = {
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-  apiKey: "AIzaSyDsdGQL-FYLsQdQipQPa8M0BCb0SHg4UTY",
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDsdGQL-FYLsQdQipQPa8M0BCb0SHg4UTY",
+    authDomain: "gen-lang-client-0173296700.firebaseapp.com",
+    projectId: "gen-lang-client-0173296700",
+    storageBucket: "gen-lang-client-0173296700.firebasestorage.app",
+    messagingSenderId: "109891504599",
+    appId: "1:109891504599:web:2d8048ed8eb17e8b436392",
+    measurementId: "G-PYV50WQD35"
+  };
 
-  authDomain: "gen-lang-client-0173296700.firebaseapp.com",
-
-  projectId: "gen-lang-client-0173296700",
-
-  storageBucket: "gen-lang-client-0173296700.firebasestorage.app",
-
-  messagingSenderId: "109891504599",
-
-  appId: "1:109891504599:web:39c7f92da4c6e937436392",
-
-  measurementId: "G-ZXYWVV4VQF"
-
-};
-
-/* INITIALIZE FIREBASE */
-
-firebase.initializeApp(firebaseConfig);
-
-const auth = firebase.auth();
-
-/* SIGNUP FUNCTION */
-
-function signup(){
-
-    const username = document.getElementById("username").value;
-
-    const email = document.getElementById("email").value;
-
-    const password = document.getElementById("password").value;
-
-    auth.createUserWithEmailAndPassword(email,password)
-
-    .then((userCredential)=>{
-
-        const user = userCredential.user;
-
-        /* SAVE USER */
-
-        localStorage.setItem("username",username);
-
-        localStorage.setItem("userEmail",user.email);
-
-        /* MOVE TO DASHBOARD */
-
-        window.location.href = "dashboard.html";
-
-    })
-
-    .catch((error)=>{
-
-        alert(error.message);
-
-    });
-
-}
-
-/* LOGIN FUNCTION */
-
-function login(){
-
-    const email = document.getElementById("email").value;
-
-    const password = document.getElementById("password").value;
-
-    auth.signInWithEmailAndPassword(email,password)
-
-    .then((userCredential)=>{
-
-        const user = userCredential.user;
-
-        localStorage.setItem("userEmail",user.email);
-
-        window.location.href = "dashboard.html";
-
-    })
-
-    .catch((error)=>{
-
-        alert(error.message);
-
-    });
-
-}
-
-/* LOGOUT FUNCTION */
-
-function logout(){
-
-    auth.signOut()
-
-    .then(()=>{
-
-        localStorage.clear();
-
-        window.location.href = "index.html";
-
-    })
-
-    .catch((error)=>{
-
-        alert(error.message);
-
-    });
-
-            }
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
